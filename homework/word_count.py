f3ab51057d6656cb3403bffbe6ee302fe415f726
Fer. <lulopeze@unal.edu.co>
2025-09-24 08:31:18 -0500

    Update word_count.py

--- homework/word_count.py
+++ homework/word_count.py
@@ -2,107 +2,37 @@
 
 # pylint: disable=broad-exception-raised
 
-import fileinput
 import glob
-import os.path
-import time
-from itertools import groupby
+import os
 
-from toolz.itertoolz import concat, pluck
-
-
-
-def copy_raw_files_to_input_folder(n):
-    """Generate n copies of the raw files in the input folder"""
-
-    create_directory("files/input")
-
-    for file in glob.glob("files/raw/*"):
-
-        with open (file, "r", encoding="utf-8") as f:
-            text = f.read()
-
-        for i in range(1, n+1):
-            filename = f"{os.path.basename(file).split('.')[0]}_{i}.txt"
-            with open(f"files/input/{filename}", "w", encoding="utf-8") as f2:
-                f2.write(text)
+# Crea la carpeta files/input
+if os.path.exists("files/input/"):
+    for file in glob.glob("files/input/*"):
+        os.remove(file)
+else:
+    os.makedirs("files/input")
 
 
+# Crea n copias de cada uno de los archivos en files/raw/
+n = 100
 
-def load_input(input_directory):
-    """Funcion load_input"""
+for file in glob.glob("files/raw/*"):
 
-    sequence = []
-    files = glob.glob(f"{input_directory}/*")
-    with fileinput.input(files=files) as f:
-        for line in f:
-            sequence.append((fileinput.filename(), line))
-    return sequence
+    with open(file, "r", encoding="utf-8") as f:
+        text = f.read()
 
-
-def preprocess_line(x):
-    """Preprocess the line x"""
-
-
-def map_line(x):
-    pass
-
-def mapper(sequence):
-    """Mapper"""
-
-
-def shuffle_and_sort(sequence):
-    """Shuffle and Sort"""
+    for i in range(1, n + 1):
+        raw_filename_with_extension = os.path.basename(file)
+        raw_filename_without_extension = os.path.splitext(raw_filename_with_extension)[
+            0
+        ]
+        new_filename = f"{raw_filename_without_extension}_{i}.txt"
+        with open(f"files/input/{new_filename}", "w", encoding="utf-8") as f2:
+            f2.write(text)
 
 
 
-def compute_sum_by_group(group):
-    pass
-
-def reducer(sequence):
-    """Reducer"""
 
 
-def create_directory(directory):
-    """Create Output Directory"""
-
-    if os.path.exists(directory):
-        for file in glob.glob(f"{directory}/*"):
-            os.remove(file)
-        os.rmdir(directory)
-    
-    os.mkdir(directory)
 
 
-def save_output(output_directory, sequence):
-    """Save Output"""
-
-
-def create_marker(output_directory):
-    """Create Marker"""
-
-
-def run_job(input_directory, output_directory):
-    """Job"""
-    sequence = load_input(input_directory)
-    sequence = mapper(sequence)
-    sequence = shuffle_and_sort(sequence)
-    sequence = reducer(sequence)
-    create_directory(output_directory)
-    save_output(output_directory, sequence)
-    create_marker(output_directory)
-
-
-if __name__ == "__main__":
-
-    copy_raw_files_to_input_folder(n=2)
-
-    start_time = time.time()
-
-    run_job(
-        "files/input",
-        "files/output",
-    )
-
-    end_time = time.time()
-    print(f"Tiempo de ejecución: {end_time - start_time:.2f} segundos")
